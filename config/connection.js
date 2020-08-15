d8e58d81d44683ba207b9ac95411f06e1dff5a44
Sandy Smith <toosandra@gmail.com>
2020-08-15 15:56:26 -0400

    Updating connection file for dot env

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");    
 var connection;
+require('dotenv').config();
 
 if(process.env.JAWSDB_URL) {            
     connection = mysql.createConnection(process.env.JAWSDB_URL);
