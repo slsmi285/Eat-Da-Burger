3be76c5fb0b36e7b5de972ad718cd871b4e2ce39
Sandy Smith <toosandra@gmail.com>
2020-08-15 15:59:41 -0400

    revising connection to JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,18 @@ var mysql = require("mysql");
 var connection;
 require('dotenv').config();
 
-if(process.env.JAWSDB_URL) {            
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+//if(process.env.JAWSDB_URL) {            
+    //connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-} else {
+//} else {
   connection = mysql.createConnection({ // Set local connection parameters
-    host: "localhost",
-    user: "root",
-    password: "Florida89!",
-    database: "burgers_db",
+    host: "e11wl4mksauxgu1w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "aeoq5stbg0ayn15n",
+    password: "Florida8pcff2wboo6rnn34t9!",
+    database: "qtllgpvg135rc5om",
     dialect: "mysql"
     });
-};
+
 
 //creating connection
 connection.connect(function(err) {     
